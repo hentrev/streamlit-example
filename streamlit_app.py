59b3f6280764a332d5ccdc987789f8dfd1dc73e2
Henrik Trevisan <38217077+hentrev@users.noreply.github.com>
2024-02-29 12:27:02 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,22 @@ import pandas as pd
 import streamlit as st
 import requests
 from time import sleep
-
+from itertools import chain
 
 station_information = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_mz/de/station_information.json'
 station_status = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_mz/de/station_status.json'
+free_bike_status = 'https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_mz/de/free_bike_status.json'
 
 json_information = requests.get(station_information).json()
 json_status = requests.get(station_status).json()
+json_free_bike_status = requests.get(free_bike_status).json()
+
+df_bikes = pd.DataFrame(json_free_bike_status['data']['bikes'])
+num_bikes_available = len(df_bikes[df_bikes['is_disabled'] == False].index)
+num_bikes_unavailable = len(df_bikes[df_bikes['is_disabled'] == True].index)
+bikes_without_station = df_bikes[df_bikes['station_id'].isna()]
+num_bikes_without_station = len(bikes_without_station.index)
+
 
 df_info = pd.DataFrame(json_information['data']['stations'])
 df_status = pd.DataFrame(json_status['data']['stations'])
@@ -21,7 +30,7 @@ df_status['station_id'] = df_status['station_id'].astype(int)
 df_merged = df_info.merge(df_status, left_on='station_id', right_on='station_id')
 df_merged['num_bikes_available'] = df_merged['num_bikes_available']
 df_merged['ratio'] = (df_merged['num_bikes_available'] / df_merged['num_docks_available'])
-df_merged['ratio'].replace(np.inf, 0, inplace=True)
+df_merged['ratio'] = df_merged['ratio'].replace(np.inf, 0)
 
 def get_color(pct):
     pct_diff = 1.0 - pct
@@ -35,7 +44,19 @@ df_merged['color'] = df_merged.apply(lambda row: get_color(row['ratio']), axis=1
 empty_stations = df_merged[df_merged['num_bikes_available'] == 0]
 full_stations = df_merged[df_merged['num_bikes_available'] == df_merged['num_docks_available']]
 
-st.markdown('## Nextbike Mainz - Stationsstatus')
+st.markdown('## Nextbike Mainz - Status')
+
+st.markdown('### Räder')
+
+col1, col2, col3 = st.columns(3)
+col1.metric('Anzahl aktive Räder', num_bikes_available)
+col2.metric('Anzahl deaktivierte Räder', num_bikes_unavailable)
+col3.metric('Anzahl Räder abseits einer Station', num_bikes_without_station)
+
+st.markdown('#### Räder abseits einer Station')
+st.map(bikes_without_station, latitude='lat', longitude='lon')
+
+st.markdown('### Stationen')
 
 col1, col2, col3 = st.columns(3)
 percentage_empty_stations = str(int(((len(empty_stations) / len(df_merged))*100))) + "%"
